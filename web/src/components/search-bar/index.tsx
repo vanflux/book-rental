4123a1fa770311a4e79230971776a65d24ae4a55
vanflux <lucasabruzzi@hotmail.com>
2023-10-11 18:39:54 -0300

    feat: going to books on search bar changes

--- web/src/components/search-bar/index.tsx
+++ web/src/components/search-bar/index.tsx
@@ -1,3 +1,6 @@
+import { ChangeEvent } from 'react';
+import { matchRoutes, useLocation, useNavigate } from 'react-router-dom';
+import { routes } from '../../router/routes';
 import { Icon } from '../icons';
 import styles from './styles.module.css';
 
@@ -7,13 +10,24 @@ interface Props {
 }
 
 export function SearchBar({ value, onChange }: Props) {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const matchResult = matchRoutes([{ path: routes.BOOKS() }], location);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+    if (!matchResult?.[0]?.pathname) {
+      navigate(routes.BOOKS());
+    }
+  };
+
   return (
     <div className={styles.container}>
       <input
         className={styles.input}
         placeholder='O que você está procurando?'
         value={value ?? ''}
-        onChange={e => onChange?.(e.target.value)}
+        onChange={handleChange}
       />
       <Icon type='search' size={24} className={styles.icon} />
     </div>
